4a4086c4a81a3d60665d621e9b69eba1c6031cb9
Igor de Spirlet <73110936+IgorDeSpi@users.noreply.github.com>
2022-10-04 12:07:06 +0200

    Update philo.h

---
+++ philo_bonus/philo.h
@@ -0,0 +1,49 @@
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <stdlib.h>
+# include <time.h>
+# include <limits.h>
+# include <stdio.h>
+# include <string.h>
+# include <pthread.h>
+# include <unistd.h>
+# include <sys/time.h>
+# include <semaphore.h>
+# include <signal.h>
+# include <stdatomic.h>
+
+typedef struct	s_dinner
+{
+	pthread_t	thread;
+	int				philo_id;
+	atomic_int		meal_count;
+	atomic_long		time_last_meal;
+	int				number_of_philo;
+	int				time_to_die;
+	int				time_to_eat;
+	int				time_to_sleep;
+	atomic_int		number_of_eat;
+	long			start_time;
+	sem_t			*sem_print;
+	sem_t			*sem_forks;
+	sem_t			*sem_dead;
+	atomic_int		in_progress;
+}				t_dinner;
+
+int		check_arg(char *arg);
+
+int		exit_with_message(char *message);
+int		setup(int argc, char **argv, t_dinner *dinner);
+
+int		play_philo(t_dinner *dinner);
+
+long	get_timestamp_in_ms(long start_time);
+void	ft_sleep(unsigned long duration, t_dinner *dinner);
+
+void	print_log(t_dinner *dinner, char *message);
+void	check_dead(t_dinner *dinner);
+
+void	clear_dinner(t_dinner *dinner);
+
+#endif
